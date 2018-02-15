638f3e42cc8dbd5dbb62ae0325c9195a516a9387
lyssabreaux <34077066+lyssabreaux@users.noreply.github.com>
2018-02-14 21:49:23 -0800

    Update google-sheet.js

--- google-sheet.js
+++ google-sheet.js
@@ -24,7 +24,7 @@ $("#foo").submit(function(event){
 
     // Fire off the request to /form.php
     request = $.ajax({
-        url: "SCRIPT URL GOES HERE",
+        url: "https://script.google.com/macros/u/1/s/AKfycbwKORTOlQ2M4q4dnkONspCFHRYdQo93KdGDVba10DJkbD8PYQM/exec",
         type: "post",
         data: serializedData
     });
